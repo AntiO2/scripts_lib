a8c7541565176e1cb26891ff066cbe79c671716c
AntiO2 <antio2@qq.com>
2025-10-12 20:07:29 +0800

    Trino

--- scripts/trino_func.sh
+++ scripts/trino_func.sh
@@ -3,9 +3,10 @@ function install_trino()
     local CONNECTOR_ZIP=`readlink -f  ${TRINO_SRC}/connector/target/pixels-trino-connector-0.2.0-SNAPSHOT.zip`
 
     mvn package -f ${TRINO_SRC}/pom.xml
-
+    check_fatal_exit "failed to build pixels-trino"
     rm -rf ${TRINO_OPT}/plugin/pixels-trino-connector-0.2.0-SNAPSHOT
     unzip ${CONNECTOR_ZIP} -d ${TRINO_OPT}/plugin
+    log_info "Success to install trino"
 }
 
 alias trino="${TRINO_OPT}/bin/launcher"
